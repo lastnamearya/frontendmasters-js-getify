9bed0aaf17dae01539f3294fde244da6e60d9fdf
lastnamearya <arya.jigyasu6815@gmail.com>
2019-06-30 19:10:09 +0530

    Coercive Equality

--- app.js
+++ app.js
@@ -1,30 +1,13 @@
-var favoriteBooks = [];
+// == is about comparison when you know the Data Types
 
-function addFavoriteBook(book) {
-  // First, We'll check if the book string contains great or not, If not then we'll add this to our collection.
+var studentName1 = "Frank";
+var studentName2 = `${studentName1}`;
 
-  // Don't assume that user will always pass an String as input for Book. So Double Test it before executing.
+console.log(studentName1 == studentName2); // true
+console.log(studentName1 === studentName2); // true
 
-  if (typeof book === "string") {
-    const bookLowerCaseString = book.toLowerCase();
+var workshopEnrollment1 = 16;
+var workshopEnrollment2 = workshopEnrollment1 + 0;
 
-    if (!bookLowerCaseString.includes("great")) {
-      // Using .includes() on string, I can check if this string contains "great" as sub-string or Not.
-
-      favoriteBooks.push(book);
-    }
-
-    // No Else Here.
-  } else {
-    console.error("Please pass an String as input for Book");
-  }
-}
-
-addFavoriteBook("A Song of Ice and Fire");
-addFavoriteBook("The Great Gatsby");
-addFavoriteBook("Crime & Punishment");
-addFavoriteBook("Great Expectations");
-addFavoriteBook("You Don't Know Js");
-addFavoriteBook(1);
-
-console.log(favoriteBooks);
+console.log(workshopEnrollment1 == workshopEnrollment2); // true
+console.log(workshopEnrollment1 === workshopEnrollment2); // true
